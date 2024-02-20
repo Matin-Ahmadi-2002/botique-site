b8a82dd9c4d54b4569be5116bed82966560c88ee
Matin-Ahmadi-2002 <miranahmadi2002@gmail.com>
2024-02-20 01:05:42 -0800

    create comment section

--- js/main.js
+++ js/main.js
@@ -58,6 +58,27 @@ $('#discount_slider').owlCarousel({
         }
     }
 })
+$('#comment_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
 
 
 window.addEventListener('scroll', function() {
